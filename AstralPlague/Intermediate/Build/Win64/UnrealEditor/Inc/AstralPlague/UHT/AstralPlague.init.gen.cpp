b75d4cf3d48e29c8182f0dfa84755b9be80f491d
Cole Andrews <ColeGameDesign@gmail.com>
2024-05-23 20:50:51 -0400

    Attribute Updates

--- AstralPlague/Intermediate/Build/Win64/UnrealEditor/Inc/AstralPlague/UHT/AstralPlague.init.gen.cpp
+++ AstralPlague/Intermediate/Build/Win64/UnrealEditor/Inc/AstralPlague/UHT/AstralPlague.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeAstralPlague_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x7025748C,
-				0xC0F64E6F,
+				0xD8690E52,
+				0xE2E5FEF1,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_AstralPlague.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_AstralPlague.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_AstralPlague(Z_Construct_UPackage__Script_AstralPlague, TEXT("/Script/AstralPlague"), Z_Registration_Info_UPackage__Script_AstralPlague, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x7025748C, 0xC0F64E6F));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_AstralPlague(Z_Construct_UPackage__Script_AstralPlague, TEXT("/Script/AstralPlague"), Z_Registration_Info_UPackage__Script_AstralPlague, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xD8690E52, 0xE2E5FEF1));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- AstralPlague/Intermediate/Build/Win64/UnrealEditor/Inc/AstralPlague/UHT/AstralPlayerController.generated.h
+++ AstralPlague/Intermediate/Build/Win64/UnrealEditor/Inc/AstralPlague/UHT/AstralPlayerController.generated.h
@@ -9,23 +9,30 @@
 #include "UObject/ScriptMacros.h"
 
 PRAGMA_DISABLE_DEPRECATION_WARNINGS
+class AAstralPlayerState;
+class UAstralAbilitySystemComponent;
 #ifdef ASTRALPLAGUE_AstralPlayerController_generated_h
 #error "AstralPlayerController.generated.h already included, missing '#pragma once' in AstralPlayerController.h"
 #endif
 #define ASTRALPLAGUE_AstralPlayerController_generated_h
 
-#define FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_15_INCLASS_NO_PURE_DECLS \
+#define FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_16_RPC_WRAPPERS_NO_PURE_DECLS \
+	DECLARE_FUNCTION(execGetIsAutoRunning); \
+	DECLARE_FUNCTION(execSetIsAutoRunning); \
+	DECLARE_FUNCTION(execGetAstralAbilitySystemComponent); \
+	DECLARE_FUNCTION(execGetAstralPlayerState);
+
+
+#define FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_16_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesAAstralPlayerController(); \
 	friend struct Z_Construct_UClass_AAstralPlayerController_Statics; \
 public: \
-	DECLARE_CLASS(AAstralPlayerController, AModularPlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/AstralPlague"), NO_API) \
+	DECLARE_CLASS(AAstralPlayerController, ACommonPlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/AstralPlague"), NO_API) \
 	DECLARE_SERIALIZER(AAstralPlayerController)
 
 
-#define FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_15_ENHANCED_CONSTRUCTORS \
-	/** Standard constructor, called after all reflected properties have been initialized */ \
-	NO_API AAstralPlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+#define FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_16_ENHANCED_CONSTRUCTORS \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	AAstralPlayerController(AAstralPlayerController&&); \
@@ -37,12 +44,13 @@ public: \
 	NO_API virtual ~AAstralPlayerController();
 
 
-#define FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_12_PROLOG
-#define FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_15_GENERATED_BODY \
+#define FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_13_PROLOG
+#define FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_16_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_15_INCLASS_NO_PURE_DECLS \
-	FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_15_ENHANCED_CONSTRUCTORS \
+	FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_16_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_16_INCLASS_NO_PURE_DECLS \
+	FID_AstralPlague_Source_AstralPlague_Player_AstralPlayerController_h_16_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
